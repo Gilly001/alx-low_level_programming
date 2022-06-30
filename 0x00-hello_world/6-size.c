0e21e55c0e32d379cab352f6cb2655a28e39adf5
Gilly001 <gilbertmaina001@gmail.com>
2022-06-30 07:47:35 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,36 +8,15 @@
 int main(void)
 {
     int a;
-    long int b;
-    long long int c;
-    char d;
-    float g;
-    
-    printf("size of interger:%lu bytes \n",(sizeof(a)));
-    printf("size of long integer:%lu bytes \n", (sizeof(b)));
-    printf("sizeof long long integer:%lu bytes \n", (sizeof(c)));
-    printf("size of character: %lu bytes \n", (sizeof(d)));
-    printf("size of float: %lu bytes \n", (sizeof(g)));
-    return (0);
-}#include <stdio.h>
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    int a;
-    long int b;
-    long long int c;
-    char d;
-    float g;
-    
-    printf("size of interger:%lu bytes \n",(sizeof(a)));
-    printf("size of long integer:%lu bytes \n", (sizeof(b)));
-    printf("sizeof long long integer:%lu bytes \n", (sizeof(c)));
-    printf("size of character: %lu bytes \n", (sizeof(d)));
-    printf("size of float: %lu bytes \n", (sizeof(g)));
-    return (0);
+	printf("Size of a char: %lu byte\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu bytes\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte\n", (unsigned long)sizeof(f));
+	return (0);
 }
