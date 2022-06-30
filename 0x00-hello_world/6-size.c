fa0b959e0dc67f0bb1aa4e541186bd3e3185e8ca
Gilly001 <gilbertmaina001@gmail.com>
2022-06-30 07:38:25 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int a;
+    long int b;
+    long long int c;
+    char d;
+    float g;
+    
+    printf("size of interger:%lu bytes \n",(sizeof(a)));
+    printf("size of long integer:%lu bytes \n", (sizeof(b)));
+    printf("sizeof long long integer:%lu bytes \n", (sizeof(c)));
+    printf("size of character: %lu bytes \n", (sizeof(d)));
+    printf("size of float: %lu bytes \n", (sizeof(g)));
+    return (0);
+}#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int a;
+    long int b;
+    long long int c;
+    char d;
+    float g;
+    
+    printf("size of interger:%lu bytes \n",(sizeof(a)));
+    printf("size of long integer:%lu bytes \n", (sizeof(b)));
+    printf("sizeof long long integer:%lu bytes \n", (sizeof(c)));
+    printf("size of character: %lu bytes \n", (sizeof(d)));
+    printf("size of float: %lu bytes \n", (sizeof(g)));
+    return (0);
+}
