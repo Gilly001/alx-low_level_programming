e2bf77da7613a35de5e88cae5d78192b93f3f16b
Gilly001 <gilbertmaina001@gmail.com>
2022-07-05 07:48:23 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints 'Holberton \n'
+ * Return: Always 0 (success)
+ */
+int main()
+{
+    printf("_putchar\n");
+	return (0);
+}
