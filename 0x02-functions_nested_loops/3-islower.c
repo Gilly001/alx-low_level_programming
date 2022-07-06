2929f74dd7ef2ecdf325b055a1bec787dfa615a5
Gilly001 <gilbertmaina001@gmail.com>
2022-07-05 23:30:07 -0700

    check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: checks for lowercase
+ * Return: 1 if true. 0 if false.
+ */
+int _islower(int c)
+{
+    int a;
+    int b;
+
+    for (a = 'a'; a <= 'z'; a++)
+    {
+    /* refer int c*/
+        if (b == i)
+        {
+            return (1);
+        }
+    }
+    return (0);
+}
