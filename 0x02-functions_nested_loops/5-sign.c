a1ae809bb9b6fd333f33011f9ee4835ce78e3fe4
Gilly001 <gilbertmaina001@gmail.com>
2022-07-06 00:44:32 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isalpha - Entry point
- * Description: checks for signs
- * Return: 1 if true. 0 if false
+ * print_sign - Entry point
+ * Description: prints the sign of a number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
  */
 
 int print_sign(int n)
@@ -11,16 +11,13 @@ int print_sign(int n)
     if (n > 0)
     {
         _putchar('+');
-        return(1);
-    }
-    else if (n = 0)
-    {
-        _putchar('0');
-        return(0);
+        return (1);
     }
     else if (n < 0)
     {
         _putchar('-');
-        return(-1);
+        return (-1);
     }
+    _putchar('0');
+    return (0);
 }
