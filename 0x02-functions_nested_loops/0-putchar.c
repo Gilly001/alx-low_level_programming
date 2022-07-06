a21a7cd6a344eabea89fc097c28c84e5125acde2
Gilly001 <gilbertmaina001@gmail.com>
2022-07-05 22:45:01 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * _putchar - writes the character c to stdout
- * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int main()
 {
     printf("_putchar\n");
-	return (0);
+    return (0);
 }
