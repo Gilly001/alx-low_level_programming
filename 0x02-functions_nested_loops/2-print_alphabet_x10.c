5b8911b4ae6655ab753ddaa9a3d8a069c4b1a3b3
Gilly001 <gilbertmaina001@gmail.com>
2022-07-05 23:10:59 -0700

    alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabetx10,
+ * in lowecase followed by a new line
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+    int a = 0;
+    int b;
+    
+    while (a < 10)
+    {
+        for (b = 'a'; b <= 'z'; b++)
+        {
+            putchar(b);
+        }
+        a++;
+    }
+    putchar('\n');
+}
