3c6440c2b803481335913a12d9079515ca9555f6
Gilly001 <gilbertmaina001@gmail.com>
2022-07-07 06:50:39 -0700

    check for upercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - check for uppercase
+ * @c: integer
+ * Description: checks for uppercase letters
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+}
