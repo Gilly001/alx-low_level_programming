1cc2901407dfd9f40521b85b6c0e5f9f9e604398
Gilly001 <gilbertmaina001@gmail.com>
2022-07-12 09:27:08 -0700

    characters f a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,14 +2,29 @@
 #include <string.h>
 
 void puts2(char *str) {
-    char j[];
+    char j[100];
     int i;
-    s = &j;
+    str = &j;
     
     
     for (i = 0; i < strlen(j); i++)
     {
-        printf("%c", *s++);
+        printf("%c", *str++);
+    }
+    putchar('\n');
+    return 0;
+}#include "main.h"
+#include <string.h>
+
+void puts2(char *str) {
+    char j[100];
+    int i;
+    str = &j;
+    
+    
+    for (i = 0; i < strlen(j); i++)
+    {
+        printf("%c", *str++);
     }
     putchar('\n');
     return 0;
