3250d0d37ebc157475bf0666959cecc8dd693e1b
Gilly001 <gilbertmaina001@gmail.com>
2022-07-12 09:21:11 -0700

    characters of string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,44 +1,16 @@
 #include "main.h"
+#include <string.h>
 
-/**
- * puts2 - prints charaters of a string
- * @str: string
- * Return: 0
- */
-
-void puts2(char *str)
-{
-	int a = 0;
-
-	while (str[a] != '\0')
-	{
-		if (a % 2 == 0)
-		{
-			printf("%c", str[a]);
-		}
-		a++;
-	}
-	_putchar('\n');
-}	
-#include "main.h"
-
-/**
- * puts2 - prints charaters of a string
- * @str: string
- * Return: 0
- */
-
-void puts2(char *str)
-{
-	int a = 0;
-
-	while (str[a] != '\0')
-	{
-		if (a % 2 == 0)
-		{
-			printf("%c", str[a]);
-		}
-		a++;
-	}
-	_putchar('\n');
-}	
+void puts2(char *str) {
+    char j[];
+    int i;
+    s = &j;
+    
+    
+    for (i = 0; i < strlen(j); i++)
+    {
+        printf("%c", *s++);
+    }
+    putchar('\n');
+    return 0;
+}
