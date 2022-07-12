24ade9edefa9986d6900154f0d1d5c3e69fce486
Gilly001 <gilbertmaina001@gmail.com>
2022-07-11 19:17:51 -0700

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints string and a new line
+ * @str: pointer
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
