1ff55eaf1cf90bdb32ed690d2d10ea0e9832d589
Gilly001 <gilbertmaina001@gmail.com>
2022-07-13 13:12:59 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * _strncat - appends src to the dest string
+ * _strcat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
  *
  * Return: address of dest
  */
-char *_strncat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int x, y;
 
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while (*(src + j))
+	x = y = 0;
+	while (*(dest + x))
+		x++;
+	while ((*(dest + x) = *(src + y)))
 	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++
+		x++;
+		y++;
 	}
 	return (dest);
 }
